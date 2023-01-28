a9edaed61390d69d9c7ad3aa6b06ffcf65ca1738
keo88 <keo.kim0808@gmail.com>
2023-01-28 21:03:16 +0900

    feat: series text auto completion.

---
+++ KKVideoPlayer/Services/SeriesAutoCompleteService.cs
@@ -0,0 +1,13 @@
+﻿namespace KKVideoPlayer.Services
+{
+    using KKVideoPlayer.Foundation;
+    using KKVideoPlayer.Models;
+
+    public class SeriesAutoCompleteService : AutoCompleteTextBoxService<VideoProperty>
+    {
+        public SeriesAutoCompleteService()
+        {
+            VideoPropertyItems = VideosCollection.PropertiesDict[VideoPropertyEnum.Series].Values;
+        }
+    }
+}
